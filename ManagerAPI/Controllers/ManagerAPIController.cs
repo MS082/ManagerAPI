f247674827a5582382f8a94f7870078f756b4248
Manjot Singh <MS082@atcltd.atc.org.nz>
2019-12-11 06:51:13 +1300

    Add project files.

---
+++ ManagerAPI/Controllers/ManagerAPIController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ManagerAPI.Controllers
+{
+    public class ManagerAPIController : Controller
+    {
+        // GET: ManagerAPI
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
